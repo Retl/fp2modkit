3abdb2e53ca8182e8d1f27e927ab4a0bcc6a95d4
Retl <carlis.moore@gmail.com>
2017-02-04 12:41:37 -0500

    Avoid crashing when loading Scene-Bundles.

--- Assets/EarlyDevelopment/ModLoader.cs
+++ Assets/EarlyDevelopment/ModLoader.cs
@@ -65,77 +65,96 @@ public class ModLoader : MonoBehaviour
         //bun.laod
 
         // If we want to actually use these scripts in the other objects, we'll want to finish adding it to the assembly first.
-        TextAsset[] tas = bun.LoadAllAssets<TextAsset>();
-        System.Type[] assemblyTypes = new System.Type[1];
-        System.Collections.Generic.List<string> tasList = new System.Collections.Generic.List<string>();
-        foreach (TextAsset ta in tas) { tasList.Add(ta.name); }
-        Debug.Log("TextAssets loaded: " + string.Join(", ", tasList.ToArray()));
         TextAsset txt = null;
+        System.Type[] assemblyTypes = new System.Type[1];
+        bool isSceneBundle = false;
 
-        if (tasList.Count > 0)
+        try
         {
-            // Load the TextAsset object
-            foreach (TextAsset ta in tas)
-            {
-                txt = ta;
-                Debug.Log("taName: " + txt.name);
-                if (txt.name.Contains(".dll"))
-                {
-                    Debug.Log("txt.name: " + txt.name);
-                    Debug.Log(txt ? "Found ModAssembly. **" + txt.name : "NOPE!");
-                    //txt = bun.LoadAsset<TextAsset>("ModAssembly");
-                    //Debug.Log(txt ? "Found ModAssembly. Applying to ModMain object." : "NOPE!");
+            TextAsset[] tas = bun.LoadAllAssets<TextAsset>();
+            System.Collections.Generic.List<string> tasList = new System.Collections.Generic.List<string>();
+            foreach (TextAsset ta in tas) { tasList.Add(ta.name); }
+            Debug.Log("TextAssets loaded: " + string.Join(", ", tasList.ToArray()));
+            
 
-                    // Load the assembly and get a type (class) from it
-                    if (txt)
+            if (tasList.Count > 0)
+            {
+                // Load the TextAsset object
+                foreach (TextAsset ta in tas)
+                {
+                    txt = ta;
+                    Debug.Log("taName: " + txt.name);
+                    if (txt.name.Contains(".dll"))
                     {
-                        //Debug.Log("Found ModAssembly. Applying to ModMain object.");
-                        Debug.Log("Found ModAssembly, Loading into Current Assembly.");
-                        var asm = System.Reflection.Assembly.Load(txt.bytes);
-                        assemblyTypes = asm.GetTypes();
-                        foreach (System.Reflection.Module m in asm.GetLoadedModules()) { Debug.Log("Module: " + m.ToString()); }
-                        foreach (System.Type t in asm.GetExportedTypes()) { Debug.Log("Exported Types: " + t.ToString()); }
-                        
-                        //var type = assembly.GetType("ModMainBehavior");
-                        //GameObject go = Instantiate(bun.LoadAsset<GameObject>("ModMain"));
-                        //go.AddComponent(type);
+                        Debug.Log("txt.name: " + txt.name);
+                        Debug.Log(txt ? "Found ModAssembly. **" + txt.name : "NOPE!");
+                        //txt = bun.LoadAsset<TextAsset>("ModAssembly");
+                        //Debug.Log(txt ? "Found ModAssembly. Applying to ModMain object." : "NOPE!");
+
+                        // Load the assembly and get a type (class) from it
+                        if (txt)
+                        {
+                            //Debug.Log("Found ModAssembly. Applying to ModMain object.");
+                            Debug.Log("Found ModAssembly, Loading into Current Assembly.");
+                            var asm = System.Reflection.Assembly.Load(txt.bytes);
+                            assemblyTypes = asm.GetTypes();
+                            foreach (System.Reflection.Module m in asm.GetLoadedModules()) { Debug.Log("Module: " + m.ToString()); }
+                            foreach (System.Type t in asm.GetExportedTypes()) { Debug.Log("Exported Types: " + t.ToString()); }
+
+                            //var type = assembly.GetType("ModMainBehavior");
+                            //GameObject go = Instantiate(bun.LoadAsset<GameObject>("ModMain"));
+                            //go.AddComponent(type);
+                        }
                     }
                 }
             }
         }
-
-        UnityEngine.Object[] objs = bun.LoadAllAssets<UnityEngine.Object>();
-        System.Collections.Generic.List<string> objsList = new System.Collections.Generic.List<string>();
-        foreach (UnityEngine.Object obj in objs) { objsList.Add(obj.name); }
-        Debug.Log("ALL Objects loaded: " + string.Join(", ", objsList.ToArray()));
-
-        UnityEngine.GameObject[] gos = bun.LoadAllAssets<UnityEngine.GameObject>();
-        System.Collections.Generic.List<string> gosList = new System.Collections.Generic.List<string>();
-        foreach (UnityEngine.GameObject go in gos) { gosList.Add(go.name); }
-        Debug.Log("GameObjects loaded: " + string.Join(", ", gosList.ToArray()));
-
-        if (!txt)
+        catch (System.InvalidOperationException e) 
         {
-            Debug.Log("Did not find Assembly file.");
+            Debug.Log("Probably trying to call the operation on a bundle that's just a scene file: " + e);
+            isSceneBundle = true;
         }
 
-        foreach (GameObject g in gos)
-        {
-            if (g.name.Contains("_autospawn")) { Object.Instantiate(g); }
-        }
 
-        if (assemblyTypes.Length > 0)
+        if (!isSceneBundle)
         {
-            GameObject g = new GameObject();
-            foreach (System.Type at in assemblyTypes)
+            UnityEngine.Object[] objs = bun.LoadAllAssets<UnityEngine.Object>();
+            System.Collections.Generic.List<string> objsList = new System.Collections.Generic.List<string>();
+            foreach (UnityEngine.Object obj in objs) { objsList.Add(obj.name); }
+            Debug.Log("ALL Objects loaded: " + string.Join(", ", objsList.ToArray()));
+
+            UnityEngine.GameObject[] gos = bun.LoadAllAssets<UnityEngine.GameObject>();
+            System.Collections.Generic.List<string> gosList = new System.Collections.Generic.List<string>();
+            foreach (UnityEngine.GameObject go in gos) { gosList.Add(go.name); }
+            Debug.Log("GameObjects loaded: " + string.Join(", ", gosList.ToArray()));
+
+            if (!txt)
             {
-                Debug.Log("Checking Component: " + at.Name);
-                if (at.Name.Contains("_autoscript")) 
+                Debug.Log("Did not find Assembly file.");
+            }
+
+            foreach (GameObject g in gos)
+            {
+                if (g.name.Contains("_autospawn")) { Object.Instantiate(g); }
+            }
+
+            if (assemblyTypes.Length > 0)
+            {
+                GameObject g = new GameObject();
+                foreach (System.Type at in assemblyTypes)
                 {
-                    Debug.Log("Adding Component: " + at.Name );
-                    g.AddComponent(at); 
+                    Debug.Log("Checking Component: " + at.Name);
+                    if (at.Name.Contains("_autoscript"))
+                    {
+                        Debug.Log("Adding Component: " + at.Name);
+                        g.AddComponent(at);
+                    }
                 }
             }
+        }
+        else 
+        {
+            Debug.Log("Scene paths are probably: " + string.Join(",\n", bun.GetAllScenePaths()));
         }
     }
 }
